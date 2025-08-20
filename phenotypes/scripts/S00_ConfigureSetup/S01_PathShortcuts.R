4dbecad1887fdf8a9451181bdb43839b9cff92f0
FiG-T <finley.thomas.17@ucl.ac.uk>
2025-08-20 16:13:50 +0100

    Configure files initiated

---
+++ phenotypes/scripts/S00_ConfigureSetup/S01_PathShortcuts.R
@@ -0,0 +1,3 @@
+# Set up general paths to streamline scripts in the phenotypes repos.
+
+pheno_paths <- here::here("phenotypes")
---
+++ phenotypes/scripts/S00_ConfigureSetup/S02_PalettesAndThemes.R
@@ -0,0 +1,17 @@
+# Configure Colour Palettes
+
+aus_fly_palette <- c(
+  mM = "deepskyblue",
+  mX = "hotpink",
+  tX = "purple",
+  tT = "firebrick"
+)
+
+
+# Configure Levels
+
+# for genotypes
+genotype_factor_levels <- c("mM", "mX", "tX", "tT")
+
+# for populations
+population_factor_levels <- c("11", "1", "2", "3","4", "5", "6", "7","8", "9", "10", "14")
