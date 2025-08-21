de9fa81be041b030dcfff9c116014bf04f0a577d
FiG-T <finley.thomas.17@ucl.ac.uk>
2025-08-21 15:10:35 +0100

    Added libraries to config file

--- phenotypes/scripts/S00_ConfigureSetup/S01_PathShortcuts.R
+++ phenotypes/scripts/S00_ConfigureSetup/S01_PathShortcuts.R
@@ -1,3 +1,17 @@
 # Set up general paths to streamline scripts in the phenotypes repos.
 
 pheno_paths <- here::here("phenotypes")
+
+
+# Set Up Libraries
+
+library(here, quietly = TRUE)
+library(readxl, quietly = TRUE)
+library(dplyr, quietly = TRUE)
+library(testthat, quietly = TRUE)
+library(readr, quietly = TRUE)
+library(ggplot2, quietly = TRUE)
+
+#library(tidyr, quietly = TRUE)
+# library(stringr, quietly = TRUE)
+# library(forcats, quietly = TRUE)
