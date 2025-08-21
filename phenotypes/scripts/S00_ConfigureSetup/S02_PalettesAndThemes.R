7dd7d6669709f901827c6973b0df6ff5ec547dae
FiG-T <finley.thomas.17@ucl.ac.uk>
2025-08-21 10:28:42 +0100

    Added muted strips and updated alternate colour palette

--- phenotypes/scripts/S00_ConfigureSetup/S02_PalettesAndThemes.R
+++ phenotypes/scripts/S00_ConfigureSetup/S02_PalettesAndThemes.R
@@ -7,6 +7,12 @@ aus_fly_palette <- c(
   tT = "firebrick"
 )
 
+aus_fly_palette_alt <- c(
+  mM = "#1C77C3",
+  mX = "#EB4B98",
+  tX = "#8D15B1",
+  tT = "#8C1C13"
+)
 
 # Configure Levels
 
@@ -15,3 +21,10 @@ genotype_factor_levels <- c("mM", "mX", "tX", "tT")
 
 # for populations
 population_factor_levels <- c("11", "1", "2", "3","4", "5", "6", "7","8", "9", "10", "14")
+
+
+# Configure Themes
+muted_strips <-   theme(
+  strip.background = element_rect(fill = "#DBD4D3"),
+  strip.text = element_text(colour = "black", size = 11)
+)
