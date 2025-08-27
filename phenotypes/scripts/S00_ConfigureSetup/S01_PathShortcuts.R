45a8ec1c2a9e020ddf79ecb971d5e41d086cec23
FiG-T <finley.thomas.17@ucl.ac.uk>
2025-08-27 16:16:51 +0100

    Updates to also include factor levels

--- phenotypes/scripts/S00_ConfigureSetup/S01_PathShortcuts.R
+++ phenotypes/scripts/S00_ConfigureSetup/S01_PathShortcuts.R
@@ -1,9 +1,9 @@
-# Set up general paths to streamline scripts in the phenotypes repos.
+# Set up general paths to streamline scripts in the phenotypes repos. --------
 
 pheno_paths <- here::here("phenotypes")
 
 
-# Set Up Libraries
+# Set Up Libraries ------------------------------------------------------------
 
 library(here, quietly = TRUE)
 library(readxl, quietly = TRUE)
@@ -12,6 +12,7 @@ library(testthat, quietly = TRUE)
 library(readr, quietly = TRUE)
 library(ggplot2, quietly = TRUE)
 library(stringr, quietly = TRUE)
+library(tibble, quietly = TRUE)
 
 # for building models with fixed and random effects:
 library(lmerTest, quietly = TRUE)
@@ -19,3 +20,16 @@ library(lmerTest, quietly = TRUE)
 #library(tidyr, quietly = TRUE)
 
 # library(forcats, quietly = TRUE)
+
+
+# Configure Levels ------------------------------------------------------------
+
+# for genotypes
+genotype_factor_levels <- c("mM", "mX", "tX", "tT")
+
+# for populations
+population_factor_levels <- c("11", "1", "2", "3","4", "5", "6", "7","8", "9", "10", "14")
+
+population_factor_levels_productivity <- c(
+  "11", "12", "13",  "1", "2", "3","4", "5", "6", "7","8", "9", "10", "14", "15", "16", "17"
+)
